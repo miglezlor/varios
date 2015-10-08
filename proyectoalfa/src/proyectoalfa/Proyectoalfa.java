597daf2ca710724ea802a2934d60e63fb0d3a00d
mgonzalezlorenzo <mgonzalezlorenzo@atanasoff06.danielcastelao.org>
2015-10-08 08:55:58 +0200

    Inicial

---
+++ proyectoalfa/src/proyectoalfa/Proyectoalfa.java
@@ -0,0 +1,16 @@
+
+package proyectoalfa;
+
+/**
+ *
+ * @author mgonzalezlorenzo
+ */
+public class Proyectoalfa {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("HolaMundo");
+    }
+    
+}
