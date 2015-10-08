9b45834345bb9a27a6a320e84461c02d19b7c111
mgonzalezlorenzo <mgonzalezlorenzo@atanasoff06.danielcastelao.org>
2015-10-08 09:39:05 +0200

    final+delay

--- proyectoalfa/src/proyectoalfa/Proyectoalfa.java
+++ proyectoalfa/src/proyectoalfa/Proyectoalfa.java
@@ -8,9 +8,10 @@ package proyectoalfa;
 public class Proyectoalfa {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         System.out.println("Hola Mundo");
+        Thread.sleep(2000);
         System.out.println("Adiós Mundo");
     }
     
