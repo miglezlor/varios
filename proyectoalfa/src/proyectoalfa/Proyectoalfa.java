f56a37285683c6e6ff6ff8ea6e25f58b4f8f994d
mgonzalezlorenzo <mgonzalezlorenzo@atanasoff06.danielcastelao.org>
2015-10-19 08:55:52 +0200

    asd

--- proyectoalfa/src/proyectoalfa/Proyectoalfa.java
+++ proyectoalfa/src/proyectoalfa/Proyectoalfa.java
@@ -11,7 +11,7 @@ public class Proyectoalfa {
     public static void main(String[] args) throws InterruptedException{
 
         System.out.println("Hola Mundo");
-        Thread.sleep(1400);
+        Thread.sleep(1700);
         System.out.println("Adiós Mundo");
     }
     
